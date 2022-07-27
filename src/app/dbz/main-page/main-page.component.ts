0073a0ac21bd9993aaddfa73df5b4cf242bf7418
[baruc] <[barucgs@gmail.com]>
2022-07-27 18:52:37 -0500

    hasta 61 crear componente hijo

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,47 @@
+import { Component,} from '@angular/core';
+
+
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent {
+
+  personajes: Personaje[] = [
+    {
+      nombre: 'goku',
+      poder: 15000
+    },
+    {
+      nombre: 'vegeta',
+      poder: 8500
+    }
+  ];
+  
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  agregar( ){
+    
+    if( this.nuevo.nombre.trim().length === 0 ){ return; }
+    console.log(this.nuevo);
+
+    this.personajes.push( this.nuevo );
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+    
+  }
+
+
+
+}
